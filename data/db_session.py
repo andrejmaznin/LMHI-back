65152f848c3c9aca3ed4fefc66b5cf71b3511628
andrejmaznin <56504319+andrejmaznin@users.noreply.github.com>
2021-07-23 11:41:15 +0500

    Update db_session.py

--- data/db_session.py
+++ data/db_session.py
@@ -1,8 +1,9 @@
 import os
+
 import sqlalchemy as sa
+import sqlalchemy.ext.declarative as dec
 import sqlalchemy.orm as orm
 from sqlalchemy.orm import Session
-import sqlalchemy.ext.declarative as dec
 
 __factory = None
 SqlAlchemyBase = dec.declarative_base()
@@ -14,13 +15,11 @@ def global_init():
     if __factory:
         return
 
-    conn_str = os.environ['DATABASE_URL']
+    conn_str = "postgresql" + os.environ['DATABASE_URL'].lstrip("postgres")
     print(f"Подключение к базе данных по адресу {conn_str}")
 
     engine = sa.create_engine(conn_str, echo=False)
     __factory = orm.sessionmaker(bind=engine)
-
-    from . import __all_models
 
     SqlAlchemyBase.metadata.create_all(engine)
 
