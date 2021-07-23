068f25f72d1a037043c318229e17e4859800d856
andrejmaznin <56504319+andrejmaznin@users.noreply.github.com>
2021-07-23 11:15:58 +0500

    heroku database fixes

--- data/db_session.py
+++ data/db_session.py
@@ -7,16 +7,12 @@ __factory = None
 SqlAlchemyBase = dec.declarative_base()
 
 
-def global_init(db_file):
+def global_init():
     global __factory
 
     if __factory:
         return
 
-    if not db_file or not db_file.strip():
-        raise Exception("Необходимо указать файл базы данных.")
-
-    conn_str = f'postgresql://postgres:newPassword@25.78.29.199:5432/{db_file.strip()}'
     print(f"Подключение к базе данных по адресу {conn_str}")
 
     engine = sa.create_engine(conn_str, echo=False)
