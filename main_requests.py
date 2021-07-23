7b22bcbc0f80a01a6ec3815db65cf23f13530b03
AlmadelX <almadel@almadelx.ru>
2021-07-23 10:44:20 +0500

    Minor changes

--- main_requests.py
+++ main_requests.py
@@ -15,4 +15,4 @@ api.add_resource(MoodScaleResource, '/mood_scales')
 
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=8080)
+    app.run(host="127.0.0.1", port=8080, debug=True)
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,2 @@
 from main_requests import app
 
-if __name__ == '__main__':
-    app.run()
-
