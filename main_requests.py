cb65feca200547c64190c2587e095d39734d4094
andrejmaznin <56504319+andrejmaznin@users.noreply.github.com>
2021-07-24 17:57:23 +0500

    Update main_requests.py

--- main_requests.py
+++ main_requests.py
@@ -1,6 +1,8 @@
+from data.mood_notes_resourses import MoodNoteResource
+from data.mood_scales_resourses import MoodScaleResource
 from flask import Flask
+from flask_restful import Api
 from data import db_session
-from flask_restful import reqparse, abort, Api, Resource
 from data.users_resourses import UsersResource
 
 app = Flask(__name__)
@@ -10,6 +12,8 @@ api = Api(app)
 def main():
     db_session.global_init("lmhi")
     api.add_resource(UsersResource, '/users')
+    api.add_resource(MoodNoteResource, '/mood_notes')
+    api.add_resource(MoodScaleResource, '/mood_scales')
     app.run(host="25.19.108.155", port=8080)
 
 
