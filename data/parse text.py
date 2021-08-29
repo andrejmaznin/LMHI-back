4b3953c4ef2d211679d341305be31175d0f64076
andrejmaznin <56504319+andrejmaznin@users.noreply.github.com>
2021-08-29 17:54:02 +0500

    minor fixes

--- data/parse text.py
+++ data/parse text.py
@@ -3,4 +3,4 @@ with open("blue.txt", encoding="utf-8") as file:
     for line in file:
         line = line.split("  ")
         print(line)
-        print(post("http://localhost:8080/result", json={"code": "blue" + line[0], "info": line[1]}).json())
+        print(post("https://luscherian.herokuapp.com/result", json={"code": "blue" + line[0], "info": line[1]}).json())
--- data/text_data.py
+++ data/text_data.py
@@ -6,7 +6,7 @@ from .db_session import SqlAlchemyBase
 class Result(SqlAlchemyBase):
     __tablename__ = 'results'
 
-    code = sqlalchemy.Column(sqlalchemy.String, primary_key=True, autoincrement=True)
+    code = sqlalchemy.Column(sqlalchemy.String, primary_key=True)
     info = sqlalchemy.Column(sqlalchemy.String, nullable=False)
 
     def as_dict(self):
