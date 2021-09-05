4d4e4fa02e68d9f4c936de6fa397e1c80338ddd9
andrejmaznin <56504319+andrejmaznin@users.noreply.github.com>
2021-09-05 13:51:31 +0500

    production post request

--- data/parse text.py
+++ data/parse text.py
@@ -1,8 +1,10 @@
 from requests import post
 
-filename = "main"
-with open(filename + ".txt", encoding="utf-8") as file:
-    for line in file:
-        line = line.split("  ")
-        print(line)
-        print(post("https://luscherian.herokuapp.com/result", json={"code": filename + line[0], "info": line[1]}).json())
+filenames = ["yellow"]
+for i in filenames:
+    with open(i + ".txt", encoding="utf-8") as file:
+        for line in file:
+            line = line.split("  ")
+            print(line)
+            print(post("https://luscherian.herokuapp.com/result", json={"code": i + line[0], "info": line[1]}).json())
+    
--- data/text_data_resourses.py
+++ data/text_data_resourses.py
@@ -9,27 +9,6 @@ class TextDataResource(Resource):
     @staticmethod
     def post():
         payload = request.get_json(force=True)
-
-        session = db_session.create_session()
-        if not session.query(Result).filter(Result.code == payload["code"]).all():
-            text_block = Result(
-                code=payload['code'], info=payload["info"]
-            )
-            session.add(text_block)
-            session.commit()
-
-            response = jsonify({'success': 'OK'})
-            response.status_code = 201
-            return response
-
-        else:
-            response = jsonify({'ERROR': 'TEXT BLOCK ALREADY EXISTS'})
-            response.status_code = 400
-            return response
-
-    @staticmethod
-    def get():
-        payload = request.get_json(force=True)
         ans = []
         session = db_session.create_session()
         for i in payload["results"]:
@@ -42,4 +21,3 @@ class TextDataResource(Resource):
         response = jsonify({'success': 'OK', "results": ans})
         response.status_code = 201
         return response
-
