cd4be696750ba465b90a5edc1e15e4b54fd73d30
andrejmaznin <andrejmaznin@gmail.com>
2022-01-04 12:34:34 +0500

    hotfix

--- data/service/load_texts.py
+++ data/service/load_texts.py
@@ -1,11 +1,12 @@
 from requests import post
+import os
 
 files = ["red", "green", "blue", "yellow", "main"]
 
 for i in files:
     filename = i + ".txt"
     body = {"payload": [], "num": 0}
-    with open(filename, encoding="utf-8") as f:
+    with open(os.path.abspath(filename), encoding="utf-8") as f:
         lines = f.readlines()
         body["num"] = len(lines)
         for j in lines:
