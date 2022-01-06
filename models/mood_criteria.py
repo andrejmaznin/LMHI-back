ecaa5d3a636d6eb1a7fc5d64848fda5a65892950
andrejmaznin <andrejmaznin@gmail.com>
2022-01-06 14:37:55 +0500

    mood criteria fix

--- models/mood_criteria.py
+++ models/mood_criteria.py
@@ -6,7 +6,7 @@ from service.db_session import SqlAlchemyBase
 class MoodCriteria(SqlAlchemyBase):
     __tablename__ = 'mood_criterias'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    name = sqlalchemy.Column(sqlalchemy.String)
+    name = sqlalchemy.Column(sqlalchemy.String, unique=True)
 
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
