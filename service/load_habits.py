77426cc5aa501ea2d89be54a0684d7cc893636df
andrejmaznin <andrejmaznin@gmail.com>
2022-01-07 14:14:50 +0500

    fixes

--- service/load_habits.py
+++ service/load_habits.py
@@ -4,5 +4,5 @@ from requests import post
 
 with open("../service_schemas/load_habits.json", mode="r") as schema:
     payload = json.load(schema)
-    response = post("http://localhost:8080/beta_habits", json=payload).json()
+    response = post("https://luscherian.herokuapp.com/habits", json=payload).json()
     print(response)
